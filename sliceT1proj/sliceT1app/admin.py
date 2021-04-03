c452fb7ca4dffcd603e2f9d11fd668c63284c269
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-04-04 02:51:57 +0530

    lib source code and a demo django project

--- sliceT1proj/sliceT1app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from sliceT1app.models import fileDoc
-# Register your models here.
-
-admin.site.register(fileDoc)
--- sliceT1proj/sliceT1app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Slicet1AppConfig(AppConfig):
-    name = 'sliceT1app'
